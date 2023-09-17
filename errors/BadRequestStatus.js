802e8543007e8591c60f1d566aff04525891e0e7
Yonas3s <119069245+Yonas3s@users.noreply.github.com>
2023-09-17 13:20:13 +0300

    Delete errors directory

--- errors/BadRequestStatus.js
+++
@@ -1,8 +0,0 @@
-const { HTTP_STATUS_BAD_REQUEST } = require('http2').constants;
-
-module.exports = class BadRequestStatus extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = HTTP_STATUS_BAD_REQUEST;
-  }
-};
