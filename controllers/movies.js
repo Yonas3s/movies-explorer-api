807baeb3aed21fd2ff5591da61a1eae06169c892
Yonas3s <119069245+Yonas3s@users.noreply.github.com>
2023-10-17 22:11:04 +0300

    Update movies.js

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,94 +1,23 @@
-// const { HTTP_STATUS_OK, HTTP_STATUS_CREATED } = require('http2').constants;
-// const { default: mongoose } = require('mongoose');
-// const Movie = require('../models/movie');
-// const BadRequestStatus = require('../errors/BadRequestStatus');
-// const NotFoundStatus = require('../errors/NotFoundStatus');
-// const ForbiddenStatus = require('../errors/ForbiddenStatus');
-
-// module.exports.getMovies = (req, res, next) => {
-//   Movie.find({ owner: req.user._id })
-//     .then((cards) => res.status(HTTP_STATUS_OK).send(cards))
-//     .catch(next);
-// };
-
-// module.exports.addMovie = (req, res, next) => {
-//   const {
-//     country,
-//     director,
-//     duration,
-//     year,
-//     description,
-//     image,
-//     trailerLink,
-//     thumbnail,
-//     movieId,
-//     nameRU,
-//     nameEN,
-//   } = req.body;
-//   Movie.create({
-//     country,
-//     director,
-//     duration,
-//     year,
-//     description,
-//     image,
-//     trailerLink,
-//     thumbnail,
-//     movieId,
-//     nameRU,
-//     nameEN,
-//     owner: req.user._id,
-//   })
-//     .then((card) => res.status(HTTP_STATUS_CREATED).send(card))
-//     .catch((err) => {
-//       if (err instanceof mongoose.Error.ValidationError) {
-//         next(new BadRequestStatus(err.message));
-//       } else {
-//         next(err);
-//       }
-//     });
-// };
-
-// module.exports.deleteMovie = (req, res, next) => {
-//   Movie.findById(req.params.movieId)
-//     .orFail(new NotFoundStatus('Карточка с указанным _id не найдена.'))
-//     .then((card) => {
-//       if (!card.owner.equals(req.user._id)) {
-//         throw new ForbiddenStatus('Карточка другого пользователя.');
-//       }
-//       Movie.deleteOne(card)
-//         .then(() => {
-//           res.status(HTTP_STATUS_OK).send({ message: 'Карточка удалена.' });
-//         })
-//         .catch((err) => {
-//           next(err);
-//         });
-//     })
-//     .catch((err) => {
-//       if (err instanceof mongoose.Error.CastError) {
-//         next(new BadRequestStatus('Некорректный _id карточки.'));
-//       } else {
-//         next(err);
-//       }
-//     });
-// };
-
-/* Обработка ошибок mongoose.Error.ValidationError принимает err.message,
-это не дефолтный текст, а текст описсаный в схеме */
 const { HTTP_STATUS_OK, HTTP_STATUS_CREATED } = require('http2').constants;
-const mongoose = require('mongoose');
+const { default: mongoose } = require('mongoose');
 const Movie = require('../models/movie');
-const BadRequestError = require('../errors/BadRequestError');
-const NotFoundError = require('../errors/NotFoundError');
-const ForbiddenError = require('../errors/ForbiddenError');
+const BadRequestStatus = require('../errors/BadRequestStatus');
+const NotFoundStatus = require('../errors/NotFoundStatus');
+const ForbiddenStatus = require('../errors/ForbiddenStatus');
+
+module.exports.getMovies = (req, res, next) => {
+  Movie.find({ owner: req.user._id })
+    .then((cards) => res.status(HTTP_STATUS_OK).send(cards))
+    .catch(next);
+};
 
 module.exports.addMovie = (req, res, next) => {
   const {
     country,
     director,
     duration,
-    description,
     year,
+    description,
     image,
     trailerLink,
     thumbnail,
@@ -100,8 +29,8 @@ module.exports.addMovie = (req, res, next) => {
     country,
     director,
     duration,
-    description,
     year,
+    description,
     image,
     trailerLink,
     thumbnail,
@@ -113,44 +42,31 @@ module.exports.addMovie = (req, res, next) => {
     .then((card) => res.status(HTTP_STATUS_CREATED).send(card))
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
-        next(new BadRequestError(err.message));
+        next(new BadRequestStatus(err.message));
       } else {
         next(err);
       }
     });
 };
 
-module.exports.getMovies = (req, res, next) => {
-  Movie.find({ owner: req.user._id })
-    .then((cards) => res.status(HTTP_STATUS_OK).send(cards))
-    .catch(next);
-};
-
 module.exports.deleteMovie = (req, res, next) => {
   Movie.findById(req.params.movieId)
-    .orFail()
+    .orFail(new NotFoundStatus('Карточка с указанным _id не найдена.'))
     .then((card) => {
       if (!card.owner.equals(req.user._id)) {
-        throw new ForbiddenError('Карточка другого пользовател');
+        throw new ForbiddenStatus('Карточка другого пользователя.');
       }
       Movie.deleteOne(card)
-        .orFail()
         .then(() => {
-          res.status(HTTP_STATUS_OK).send({ message: 'Карточка удалена' });
+          res.status(HTTP_STATUS_OK).send({ message: 'Карточка удалена.' });
         })
         .catch((err) => {
-          if (err instanceof mongoose.Error.DocumentNotFoundError) {
-            next(new NotFoundError(`Карточка с _id: ${req.params.cardId} не найдена.`));
-          } else if (err instanceof mongoose.Error.CastError) {
-            next(new BadRequestError(`Некорректный _id карточки: ${req.params.cardId}`));
-          } else {
-            next(err);
-          }
+          next(err);
         });
     })
     .catch((err) => {
-      if (err instanceof mongoose.Error.DocumentNotFoundError) {
-        next(new NotFoundError(`Карточка с _id: ${req.params.cardId} не найдена.`));
+      if (err instanceof mongoose.Error.CastError) {
+        next(new BadRequestStatus('Некорректный _id карточки.'));
       } else {
         next(err);
       }
