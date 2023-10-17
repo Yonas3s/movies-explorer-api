2ad5d446e02beca1b25a9ea9c31c0d16a8e0234f
Yonas3s <119069245+Yonas3s@users.noreply.github.com>
2023-10-17 21:35:37 +0300

    Update movie.js

--- models/movie.js
+++ models/movie.js
@@ -65,7 +65,7 @@ const movieSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Поле должно быть заполнено'],
   },
-  cardId: {
+  movieId: {
     type: Number,
     required: true,
   }
