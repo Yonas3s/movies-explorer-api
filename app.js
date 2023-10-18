066a8585837df109c3b0f01bfcf5f2b986b64a9b
Yonas3s <119069245+Yonas3s@users.noreply.github.com>
2023-10-18 08:41:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
-app.use(cors());
-
 app.use(cors({
   origin: ['http://localhost:3000', 'https://api.yonasss-movies-diplom.nomoredomainsicu.ru'],
   methods: 'GET, POST, PUT, PATCH, DELETE',
