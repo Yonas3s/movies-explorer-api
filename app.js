00f455b7458caca4a29fda3892e8646aef2288d8
Yonas3s <119069245+Yonas3s@users.noreply.github.com>
2023-10-18 08:25:09 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -13,16 +13,22 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
-
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3001'],
+  methods: 'GET, POST, PUT, PATCH, DELETE',
+  allowedHeaders: ['Content-type', 'Authorization'],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+}));
 
 // Установить заголовки для конкретного источника
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000, https://api.yonasss-movies-diplom.nomoredomainsicu.ru');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
