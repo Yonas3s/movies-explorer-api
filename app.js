b28cf09cbe49ebfe67b16e5cd899632255972142
Yonas3s <119069245+Yonas3s@users.noreply.github.com>
2023-10-18 08:28:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,14 +13,16 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
-app.use(cors({
-  origin: ['http://localhost:3001'],
-  methods: 'GET, POST, PUT, PATCH, DELETE',
-  allowedHeaders: ['Content-type', 'Authorization'],
-  credentials: true,
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-}));
+app.use(cors());
+
+// app.use(cors({
+//   origin: ['http://localhost:3001'],
+//   methods: 'GET, POST, PUT, PATCH, DELETE',
+//   allowedHeaders: ['Content-type', 'Authorization'],
+//   credentials: true,
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+// }));
 
 // Установить заголовки для конкретного источника
 // app.use((req, res, next) => {
