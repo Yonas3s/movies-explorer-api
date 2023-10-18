ce364aefa94b13d2e59ae1733f35ef2e41fad499
Yonas3s <119069245+Yonas3s@users.noreply.github.com>
2023-10-18 07:54:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,16 +14,15 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process
 
 const app = express();
 
-app.use(
-  cors({
-  origin: ['https://api.yonasss-movies-diplom.nomoredomainsicu.ru', 'http://localhost:3000'],
-  methods: 'GET, HEAD, PUT, PUTCH, POST, DELETE',
-  allowedHeaders: ['Content-type', 'Authorization'],
-  // credentials: true,
-  // preflightContinue: false,
-  // optionsSuccessStatus: 204,
-})
-);
+app.use(cors());
+
+// Установить заголовки для конкретного источника
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
